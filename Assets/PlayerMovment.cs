4a60bb89cc0b14f174833f50c1c95e0850ac59f4
shaharlitvin2000 <shahar070510@gmail.com>
2026-02-15 16:29:40 +0200

    Added player movement

---
+++ Assets/PlayerMovment.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
